2a4d8f68589bb2ed6997364d86c667eaf3935167
haseo <colin0207@naver.com>
2025-02-09 13:10:27 +0900

    first upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Components/Header';
+import Login from './Components/Login';
+
+const App = () => {
+  return (
+    <>
+    <Header /> 
+    <main className="main-container">
+    <Login />
+    </main>
+    </>  
+  )
+}
+
+export default App;
---
+++ src/Components/Login.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import "./Login.css"
+import logo from '../images/PlayTalk.png';
+const Login = () => {
+    return (
+        <>
+            <div className='login-frame'>
+                <div className='login-info'>
+                    <div className='login-icon'>
+                        <img src={logo} alt="메인아이콘" height="50px" />
+                        <div className="icon-font">LOGIN</div>
+                    </div>
+
+                    <div className='login-id'>
+                        <label htmlFor="id">아이디</label>
+                        <input
+                            type="text"
+                            className="input-id"
+                            id="id"
+                            name="id"
+                        />
+                    </div>
+
+                    <div className='login-pw'>
+                        <label htmlFor="pw">비밀번호</label>
+                        <input
+                            type="password"
+                            className="input-pw"
+                            id="pw"
+                            name="pw"
+                        />
+                    </div>
+
+                    <div className='login-check'>
+                        <input type="checkbox" id="keeplogin" name="horns" />
+                        <label htmlFor="keeplogin">일주일간 로그인 유지하기</label>
+                    </div>
+
+                    <button className='btn__login'>
+                        로그인
+                    </button>
+
+                    <div className='find-idpw'>
+                    <a href="javascript:void(0)">아이디/비밀번호 찾기</a>
+                    </div>    
+                    
+                </div>
+            </div>
+        </>
+
+    )
+}
+
+export default Login
